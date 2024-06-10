70899ea1cf73051bf88160d00239e9849d7ea4fb
Ivan <123@gmail.com>
2024-06-10 19:46:32 +0100

    GitHub ckeck out

--- hom work 19.py
+++ hom work 19.py
@@ -1,6 +1,7 @@
 array = [int(input('What Word?\n')) for x in range(5)]
 array_ = []
 print(array_)
+print('Useless')
 for a in range(len(array)):
     if array[a-1] < array[a]:
         array_.append(array[a])
